ca9829a417cbee86641b1af181a0ac8f85df997d
aitor <aitor@localhost.localdomain>
2019-03-07 11:54:18 +0100

    añadido add file.tar

--- net18:nethost/install.sh
+++ net18:nethost/install.sh
@@ -2,5 +2,5 @@
 
 echo "web de prueba edt asixm011 2018-19" > /var/www/html/index.html
 #buscar en docker orden add que coge un archivo comprimido y lo descomprime en donde le digamos
-cp -rf /opt/docker/* /etc/xinetd.d/
+#cp -rf /opt/docker/* /etc/xinetd.d/
 
