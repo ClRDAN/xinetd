c327970af2ddf0a49b1522458b3df908c8df8bfb
isx78745231 <isx78745231@i07.informatica.escoladeltreball.org>
2019-03-07 09:46:52 +0100

    sss

--- net18:nethost/install.sh
+++ net18:nethost/install.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
 echo "web de prueba edt asixm011 2018-19" > /var/www/html/index.html
+#buscar en docker orden add que coge un archivo comprimido y lo descomprime en donde le digamos
+cp -rf /opt/docker/* /etc/xinetd.d/
+
